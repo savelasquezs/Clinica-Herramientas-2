9d3ced8a5bfc755fffd1020b6c1e9b78e049c8c7
Marlon Giraldo <marlon.giral1511@gmail.com>
2025-08-25 18:51:54 -0500

    Add class Person, User and Contact

--- Domain/Model/Person.cs
+++ Domain/Model/Person.cs
@@ -8,5 +8,15 @@ namespace Clinica_Herramientas_2.Domain.Model
 {
     internal class Person
     {
+
+        private string name;
+        private string dni;
+        private string email;
+        private string cellphone;
+
+        public string Name { get => name; set => name = value; }
+        public string Dni { get => dni; set => dni = value; }
+        public string Email { get => email; set => email = value; }
+        public string Cellphone { get => cellphone; set => cellphone = value; }
     }
 }
---
+++ Domain/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica_Herramientas_2.Domain.Model
+{
+    class User:Person
+    {
+        private string rol;
+        private string username;
+        private string password;
+
+        public string Rol { get => rol; set => rol = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
