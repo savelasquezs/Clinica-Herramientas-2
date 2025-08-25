28128423c2852dbe29322ea35660b1efde00afc5
Santiago <santyvano@outlook.com>
2025-08-25 18:26:31 -0500

    Add internal Person class to Domain.Model namespace
    Introduces a new `Person` class in the `Clinica_Herramientas_2.Domain.Model` namespace. The class is marked as internal for assembly-level access. Several using directives for common namespaces are also added for potential future use.

---
+++ Domain/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica_Herramientas_2.Domain.Model
+{
+    internal class Person
+    {
+    }
+}
